924d61ad7e9a27e12de82cf5c98ed623a7ce2519
Cian Dunne <20091471@mail.wit.ie>
2021-03-20 11:20:30 +0000

    removed content of lab 4 and reworked lab 5c instead to my necessities. Have removed all aspects of "donate" except the add donation which will be removed next.

---
+++ assignment1-island-poi-webapp/app/controllers/pois.js
@@ -0,0 +1,40 @@
+"use strict";
+const POI = require("../models/poi");
+const User = require("../models/user");
+
+const POIs = {
+  home: {
+    handler: function(request, h) {
+      return h.view("home", { title: "Home" });
+    }
+  },
+  report: {
+    handler: async function(request, h) {
+      const pois = await POI.find().populate("creator").lean();
+      return h.view("report", {
+        title: "POIs Added",
+        pois: pois
+      });
+    }
+  },
+  addPOI: {
+    handler: async function(request, h) {
+      try {
+        const id = request.auth.credentials.id;
+        const user = await User.findById(id);
+        const data = request.payload;
+        const newPOI = new POI({
+          name: data.name,
+          description: data.description,
+          creator: user._id
+        });
+        await newDonation.save();
+        return h.redirect("/report");
+      } catch (err) {
+        return h.view("main", { errors: [{ message: err.message }] });
+      }
+    }
+  }
+};
+
+module.exports = POIs;
